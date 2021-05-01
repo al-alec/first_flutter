2b55b8cf4b27d3a6738b0fe42182a38c5d288763
alec <yehadjialexis@gmail.com>
2021-05-01 16:55:07 +0000

    ch

--- lib/home.dart
+++ lib/home.dart
@@ -37,7 +37,7 @@ class _HomeState extends State<Home> {
           children: [
             Text("Hello,", style: theme.textTheme.headline2),
             Text(
-              "Lamar Jr.",
+              "alec Jr.",
               style: theme.textTheme.headline1,
             ),
             SizedBox(
